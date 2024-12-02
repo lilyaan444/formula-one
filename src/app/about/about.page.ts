417ad4cf7f85a5f2bff33cba5dda4e230c847dcd
lilyaan444 <73063658+lilyaan444@users.noreply.github.com>
2024-12-02 16:48:07 +0100

    Tab bar fix + "About" Re-design

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
 })
-export class AboutPage implements OnInit {
-  constructor() {}
+export class AboutPage {
+  constructor(private alertController: AlertController) {}
 
-  ngOnInit() {}
+  async contact() {
+    const alert = await this.alertController.create({
+      header: 'Contact',
+      message:
+        "Vous pouvez me contacter à l'adresse email suivante : mullerlilyan@proton.me",
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 }
